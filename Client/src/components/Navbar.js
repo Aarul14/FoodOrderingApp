d14219ec7992e10dfb8da72aa2e97fff23bf8bd4
Aarul Mishra <52631289+Aarul14@users.noreply.github.com>
2023-07-29 11:14:43 +0530

    Update Navbar.js

--- Client/src/components/Navbar.js
+++ Client/src/components/Navbar.js
@@ -16,7 +16,7 @@ export const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-success ">
         <div className="container-fluid">
-          <Link className="navbar-brand fs-1 fst-italic" to="/">FOODAPP</Link>
+          <Link className="navbar-brand fs-1 fst-italic" to="/">YumHub</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -58,48 +58,7 @@ export const Navbar = () => {
 
 export default Navbar;
 
-// import React from 'react'
-// import { Link, useNavigate } from "react-router-dom";
 
-// export const Navbar = () => {
-//   // let navigate = useNavigate()
-//   // const handleLogout = () => {
-//   //   localStorage.removeItem('authToken');
-//   //   navigate("/login")
-//   // }
-//   return (
-//     <div>
-//       <nav className="navbar navbar-expand-lg navbar-dark bg-success ">
-//         <div className="container-fluid">
-//           <Link className="navbar-brand fs-1 fst-italic" to="/">FOODAPP</Link>
-//           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//             <span className="navbar-toggler-icon"></span>
-//           </button>
-//           <div className="collapse navbar-collapse" id="navbarNav">
-//             <ul className="navbar-nav me-auto mb-2">
-//               <li className="nav-item">
-//                 <Link className="nav-link active fs-5" aria-current="page" to="/">Home</Link>
-//               </li>
-
-//               {(localStorage.getItem("token")) ?
-//                 <li className="nav-item">
-//                   <Link className="nav-link active fs-5" aria-current="page" to="/">My Orders</Link>
-//                 </li>
-//                 : ""}
-
-//             </ul>
-
-//             <div className='d-flex'>
-//               <Link className="btn bg-white text-success mx-1" to="/login">Login</Link>
-//               <Link className="btn bg-white text-success mx-1" to="/createuser">SignUp</Link>
-//             </div>
-
-//           </div>
-//         </div>
-//       </nav>
-//     </div>
-//   )
-// }
 
 
 
