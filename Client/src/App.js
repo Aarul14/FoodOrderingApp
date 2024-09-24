337c46dcabad187f5a787b9a19ac879cbca6755d
Aarul Mishra <52631289+Aarul14@users.noreply.github.com>
2024-09-25 00:27:33 +0530

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -8,7 +8,7 @@ import {
 } from "react-router-dom";
 import { Login } from './screens/Login';
 import { Signup } from './screens/Signup';
-import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-night.min.css'  //npm i bootstrap-dark-5 boostrap
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-night.min.css'  
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import { CartProvider } from './components/ContextReducer';
