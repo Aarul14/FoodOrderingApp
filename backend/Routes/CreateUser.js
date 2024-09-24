b7647520a4908b893436d0d060bd59decf70b1c4
Aarul Mishra <52631289+Aarul14@users.noreply.github.com>
2024-09-25 00:24:59 +0530

    Update CreateUser.js

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -4,7 +4,7 @@ const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs')
-const jwtSecret = "MyNameisAarulMishra$#"
+const jwtSecret = "MyNameisAarul$#"
 
 router.post("/createuser", [
     body('email').isEmail(),
