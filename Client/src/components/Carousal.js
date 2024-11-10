a3a7bd172151a539433f22586520914569411a31
Aarul14 <mishraaarul@gmail.com>
2024-11-10 18:55:52 +0530

    caraousal component

--- Client/src/components/Carousal.js
+++ Client/src/components/Carousal.js
@@ -1,32 +1,4 @@
-// import React from 'react'
 
-// export const Carousal = () => {
-//     return (
-//         <div>
-//             <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
-//                 <div className="carousel-inner">
-//                     <div className="carousel-item active">
-//                         <img src="https://source.unsplash.com/random/900×700/?burger" className="d-block w-100" alt="..." />
-//                     </div>
-//                     <div className="carousel-item">
-//                         <img src="https://source.unsplash.com/random/900×700/?pastry" className="d-block w-100" alt="..." />
-//                     </div>
-//                     <div className="carousel-item">
-//                         <img src="https://source.unsplash.com/random/900×700/?barbeque" className="d-block w-100" alt="..." />
-//                     </div>
-//                 </div>
-//                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-//                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-//                     <span className="visually-hidden">Previous</span>
-//                 </button>
-//                 <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-//                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
-//                     <span className="visually-hidden">Next</span>
-//                 </button>
-//             </div>
-//         </div>
-//     )
-// }
 
 import React from 'react'
 
@@ -44,13 +16,13 @@ export const Carousal = () => {
                         </form>
                     </div>
                     <div className="carousel-item active" >
-                        <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="..." />
+                        <img src="https://img.freepik.com/free-vector/twitch-banner-template-traditional-italian-food-restaurant_23-2150228281.jpg" className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://source.unsplash.com/random/900x700/?pastry" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+                        <img src="https://img.freepik.com/free-vector/flat-design-food-twitter-header_23-2149173455.jpg" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://source.unsplash.com/random/900x700/?barbeque" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+                        <img src="https://img.freepik.com/free-psd/delicious-burger-food-menu-facebook-cover-banner-template_106176-4697.jpg" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
